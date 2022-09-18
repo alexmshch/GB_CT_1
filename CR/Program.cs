0595e8af75b3b8f5d641a59d3e8d286e3ae69806
alexey <alexm171002@mail.ru>
2022-09-18 18:58:20 +0300

    program made

--- CR/Program.cs
+++ CR/Program.cs
@@ -2,4 +2,16 @@
 string[] Array2 = new string[4] {"1234", "1567","-2", "computer science"};
 string[] Array3 = new string[3] {"Russia", "Denmark", "Kazan"};
 
+string[] Arrayanswer = new string[Array1.Length];
+int b = 0;
+
+for (int i = 0; i < Array1.Length; i++) {
+    if (Array1[i].Length <= 3) {
+        Arrayanswer[b] = Array1[i];
+        Console.Write(Arrayanswer[b]);
+        Console.Write(" ");
+        b++;
+}
+}
+Console.WriteLine();
 
