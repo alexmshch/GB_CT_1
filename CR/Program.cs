a32fb6ca9df5043332758413f626271ed2a5e022
alexey <alexm171002@mail.ru>
2022-09-18 18:28:50 +0300

    added names

--- CR/Program.cs
+++ CR/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] Array2 = new string[4] {"1234", "1567","-2", "computer science"};
+string[] Array3 = new string[3] {"Russia", "Denmark", "Kazan"};
+
+
